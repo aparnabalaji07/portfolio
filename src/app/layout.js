3911c131327c7b69eedcb9832eb6f9f8d96ace59
aparnabalaji07 <aparna.balaji@gmail.com>
2026-09-17 18:36:02 -0500

    fix: suppressed the expected hydration warning caused by the theme script

--- src/app/layout.js
+++ src/app/layout.js
@@ -51,6 +51,7 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">
         <Script id="theme-init" strategy="beforeInteractive">
