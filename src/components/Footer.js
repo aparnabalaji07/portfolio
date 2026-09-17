7eaa6e4225c62512f9876ac4c035cb93ae20297b
aparnabalaji07 <aparna.balaji@gmail.com>
2026-09-17 18:03:31 -0500

    feat: added a small footer

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="border-t border-text/10 py-6 text-center">
+      <p className="text-muted text-xs">made with 🫶</p>
+    </footer>
+  );
+}
